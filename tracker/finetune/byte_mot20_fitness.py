c4fc1a7e708942304d26167aab1b2bb5ced4978e
inakierregueab <inaki.erregue@gmail.com>
2024-05-10 07:37:42 +0200

    Add hyperparameter tuning

--- tracker/finetune/byte_mot20_fitness.py
+++ tracker/finetune/byte_mot20_fitness.py
@@ -1,2 +1,117 @@
+import json
+import os.path
+import time
+import random
+import string
+
+from tracker.evaluation.generate_tracks import generate_tracks
+from tracker.evaluation.evaluate import trackeval
+
+
+def generate_unique_tag():
+    timestamp = int(time.time())  # Current Unix timestamp
+    random_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=6))  # Random 6-character alphanumeric string
+    tag = f"exp_{timestamp}_{random_suffix}"
+    return tag
+
+
 def fitness_fn(ga_instance, solution, solution_idx):
-    return None
+    # Prepare the parameters for the tracker
+
+    # Read common config
+    with open("./cfg/evolve.json", "r") as f:
+        config = json.load(f)
+
+    # Update the config with the solution
+    tracker_config = config["tracker_args"]
+    for gene_value, gene_key in zip(solution, tracker_config):
+        tracker_config[gene_key] = gene_value
+
+    # Perform Inference on the dataset
+    experiment_id = generate_unique_tag()
+    processor = generate_tracks(
+        config=config,
+        experiment_id=experiment_id
+    )
+
+    # Save the tracker config
+    trackers_folder = os.path.abspath("./outputs/tracks/" + processor.dataset)
+    trackers_to_eval = processor.experiment_name
+    json_path = trackers_folder + "/" + trackers_to_eval + "/config.json"
+    with open(json_path, "w") as f:
+        json.dump(tracker_config, f)
+
+    # Run evaluation
+    combined_metrics = trackeval(
+        evaluator_folder=os.path.abspath("./../evaluation/TrackEval"),
+        dataset_folder=config["source_gt_dir"],
+        trackers_folder=trackers_folder,
+        trackers_to_eval=trackers_to_eval,
+        metrics=["HOTA"], # uncomment this line to only evaluate HOTA, single-object fitness
+    )
+    print(combined_metrics)
+    return combined_metrics["HOTA"]#list(combined_metrics.values())
+
+
+def optuna_fitness_fn(trial):
+    # Prepare the parameters for the tracker
+
+    # Read common config
+    with open("./cfg/evolve.json", "r") as f:
+        config = json.load(f)
+
+    # Update the config with the solution
+    tracker_config = config["tracker_args"]
+    tracker_config["track_high_thresh"] = trial.suggest_float("track_high_thresh", 0.2, 0.9, step=0.01)
+    tracker_config["track_low_thresh"] = trial.suggest_float("track_low_thresh", 0.1, 0.4, step=0.01)
+    tracker_config["new_track_thresh"] = trial.suggest_float("new_track_thresh", 0.2, 0.6, step=0.01)
+    tracker_config["first_match_thresh"] = trial.suggest_float("first_match_thresh", 0.2, 1.0, step=0.01)
+    tracker_config["second_match_thresh"] = trial.suggest_float("second_match_thresh", 0.2, 1.0, step=0.01)
+    tracker_config["new_match_thresh"] = trial.suggest_float("new_match_thresh", 0.2, 1.0, step=0.01)
+    tracker_config["first_buffer"] = trial.suggest_float("first_buffer", 0.0, 0.5, step=0.01)
+    tracker_config["second_buffer"] = trial.suggest_float("second_buffer", 0.0, 0.5, step=0.01)
+    tracker_config["new_buffer"] = trial.suggest_float("new_buffer", 0.0, 0.5, step=0.01)
+    tracker_config["first_fuse"] = trial.suggest_int("first_fuse", 0, 1)
+    tracker_config["second_fuse"] = trial.suggest_int("second_fuse", 0, 1)
+    tracker_config["new_fuse"] = trial.suggest_int("new_fuse", 0, 1)
+    tracker_config["first_iou"] = trial.suggest_int("first_iou", 0, 5)
+    tracker_config["second_iou"] = trial.suggest_int("second_iou", 0, 5)
+    tracker_config["new_iou"] = trial.suggest_int("new_iou", 0, 5)
+    tracker_config["cw_thresh"] = trial.suggest_float("cw_thresh", 0.0, 1.0, step=0.01)
+    tracker_config["nk_flag"] = trial.suggest_int("nk_flag", 0, 1)
+    tracker_config["nk_alpha"] = trial.suggest_int("nk_alpha", 0, 150, step=10)
+    tracker_config["track_buffer"] = trial.suggest_int("track_buffer", 0, 500, step=50)
+
+    # Perform Inference on the dataset
+    experiment_id = generate_unique_tag()
+    processor = generate_tracks(
+        config=config,
+        experiment_id=experiment_id
+    )
+
+    # Save the tracker config
+    trackers_folder = os.path.abspath("./outputs/tracks/" + processor.dataset)
+    trackers_to_eval = processor.experiment_name
+    json_path = trackers_folder + "/" + trackers_to_eval + "/config.json"
+    with open(json_path, "w") as f:
+        json.dump(tracker_config, f)
+
+    # Run evaluation
+    combined_metrics = trackeval(
+        evaluator_folder=os.path.abspath("./../evaluation/TrackEval"),
+        dataset_folder=config["source_gt_dir"],
+        trackers_folder=trackers_folder,
+        trackers_to_eval=trackers_to_eval,
+        metrics=["HOTA"],  # uncomment this line to only evaluate HOTA, single-object fitness
+    )
+    #print(combined_metrics)
+    #return combined_metrics["HOTA"]
+    return list(combined_metrics.values())
+
+
+if __name__ == "__main__":
+    solution = [0.6, 0.3]
+    h = fitness_fn(None, solution, 0)
+    #h = optuna_fitness_fn(optuna.trial.Trial())
+    print(h)
+
--- tracker/finetune/mot_GA.py
+++ tracker/finetune/mot_GA.py
@@ -2,37 +2,46 @@ import time
 import pygad as ga
 from byte_mot20_fitness import fitness_fn
 
+
 # Callback function to be called after each generation for printing the best solution
-last_fitness = 0
 def on_generation(ga_instance):
-    global last_fitness
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
-    print("Solution   = {solution}".format(solution=ga_instance.best_solution()[0]))
     print("\n")
-    last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
+    print(f"Generation = {ga_instance.generations_completed}")
+    print(f"Fitness    = {ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]}")
+    print(f"Solution   = {ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[0]}")
+    print("\n")
+
 
 # Prepare the PyGAD parameters
 ga_params = {
     "num_generations": 100,
-    "sol_per_pop": 20,
+    "sol_per_pop": 6,
+    "keep_elitism": 2,
 
-    "num_genes": 10,
-    "gene_type": int,
-    "gene_space": [0, 1],
+    "num_genes": 19,
+    "gene_type": [float] * 9 + [int] * 3 + [int] * 3 + [float] + [int] + [int] + [int],
+    "gene_space": [{'low': 0.2, 'high': 0.9, 'step': 0.01}] + \
+                  [{'low': 0.1, 'high': 0.4, 'step': 0.01}] * 2 + \
+                  [{'low': 0.2, 'high': 0.9, 'step': 0.01}] * 3 + \
+                  [{'low': 0, 'high': 0.5, 'step': 0.01}] * 3 + \
+                  [[0, 1]] * 3 + \
+                  [[0, 1, 2, 3, 4, 5]] * 3 + \
+                  [{'low': 0, 'high': 1, 'step': 0.01}] + \
+                  [[0, 1]] + \
+                  [range(1, 151, 10)] + \
+                  [range(1, 501, 10)],
 
-    "parent_selection_type": "sss",  # "rank" is also a good choice
-    "num_parents_mating": 10,
-    "crossover_type": "single_point",  # "uniform" is also a good choice
-    "crossover_probability": None,
-    "mutation_type": "random",  # "adaptive" is also a good choice
-    "mutation_probability": 0.1,
+    "parent_selection_type": "sss",  # "nsga2" is also a good choice
+    "num_parents_mating": 2,
+    "crossover_type": "uniform",  # "uniform" is also a good choice
+    "crossover_probability": 0.5,
+    "mutation_type": "adaptive",  # "adaptive" is also a good choice
+    "mutation_probability": [0.25, 0.1],
 
     "fitness_func": fitness_fn,
     "on_generation": on_generation,
     "save_solutions": True,
-    "stop_criteria": None,
+    "stop_criteria": "saturate_10",
     "parallel_processing": None,
     "random_seed": 42,
 }
@@ -44,7 +53,7 @@ ga_instance = ga.GA(**ga_params)
 ga_instance.run()
 
 # Save the GA instance
-ga_instance.save(filename="byte_mot20_" + str(time.time()) + ".pkl")
+ga_instance.save(filename="./outputs/studies/byte_mot20_" + str(time.time()))
 
 # Print summary
 ga_instance.summary()
