4f339cbf6b23675fa8a05ab540e48dd585a35104
inakierregueab <inaki.erregue@gmail.com>
2024-05-16 17:03:52 +0200

    Delete not useful scripts

--- tracker/export.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-export_config = {'format': 'engine', 'args': {'imgsz': 640, 'half': True, 'dynamic': False, 'int8': False, 'simplify': True}}
-
-model = YOLO("./detectors/8sp2_150e_64b.pt")
-model.export(format=export_config['format'], device="cuda", **export_config['args'], project='./detectors/')
