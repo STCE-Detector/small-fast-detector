936ea5e31e79bed47104642d8c7d95a7f38859e7
johnnynunez <johnnynuca14@gmail.com>
2024-05-31 10:05:01 +0200

    .

--- tracker/export.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-config_export = {'format': 'engine', 'args': {'imgsz': 640, 'half': True, 'dynamic': False, 'int8': False, 'simplify': True}}
-
-
-model = YOLO('./detectors/8sp2_150e_64b.pt', task='detect')
-model.export(format=config_export['format'], device='cuda', **config_export['args'], project='./detectors/')
