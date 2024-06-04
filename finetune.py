45e5d6a78913f83a983626d777bf04602985ec64
inakierregueab <inaki.erregue@gmail.com>
2024-06-04 08:30:50 +0200

    New fine-tune experiment

--- finetune.py
+++ finetune.py
@@ -15,27 +15,27 @@ os.environ['NUMEXPR_NUM_THREADS'] = N_THREADS
 comet_ml.init()
 
 # Initialize model and load matching weights
-model = YOLO('yolov8s-p2-emb.yaml', task='detectemb').load('./../models/yolov8s.pt')
+model = YOLO('yolov8s-p2.yaml', task='detect').load('./../models/yolov8s.pt')
 
-epochs = 50
-batch = 16
+epochs = 300
+batch = 64
 optimizer = 'auto'
 
 model.train(
     resume=False,
-    data='custom_dataset-tagged.yaml',
+    data='custom_dataset.yaml',
     epochs=epochs,
     batch=batch,
     optimizer=optimizer,
     save=True,
-    close_mosaic=5,
+    #close_mosaic=5,
     #fraction=0.5,
     save_json=True,
     plots=True,
-    device=[3,4],
-    project='reid-embeddings',
+    device=[0,1,2,7],
+    project='fine-tune-cdv3',
     imgsz=640,
-    name=f'hm05_bh_y0',
+    name=f'8sp2-300e-64b-{optimizer}',
     verbose=True,
     patience=25,
     cache=False,
