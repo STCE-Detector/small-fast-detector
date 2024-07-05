eb17e13bd0b4dd62b5d6fd2d493fc2caac59f845
inakierregueab <inaki.erregue@gmail.com>
2024-07-05 08:26:35 +0200

    Upgrade AR gridsearch

--- tracker/ar_tools/evolve.py
+++ tracker/ar_tools/evolve.py
@@ -10,22 +10,69 @@ from tracker.ar_tools.generate_behaviors import generate_behaviors
 from tracker.finetune.evolve import generate_unique_tag
 
 
-def ar_optuna_fitness_fn(trial, config):
+def ar_optuna_fitness_fn(trial, config, objective, param_names):
     ###############################
     # OVERRIDE CONFIG WITH SOLUTION
     ###############################
 
+    objective = config["evolve"]["objective"]
+    include_projection = config["evolve"]["projection"]
+    include_buffer = config["evolve"]["buffer"]
+
     # Update the config with the solution
-    # TODO: set appropriate values for the parameters
     ar_config = config["action_recognition"]
-    ar_config["speed_projection"][0] = trial.suggest_float("speed_projection_x", 0.5, 2.0)
-    ar_config["speed_projection"][1] = trial.suggest_float("speed_projection_y", 0.5, 2.0)
-    ar_config["gather"]["distance_threshold"] = trial.suggest_float("g_distance_threshold", 0.1, 1.5)
-    ar_config["gather"]["area_threshold"] = trial.suggest_float("g_area_threshold", 0.1, 1.0)
-    ar_config["gather"]["speed_threshold"] = trial.suggest_float("g_speed_threshold", 0.0001, 0.1)
-    ar_config["stand_still"]["speed_threshold"] = trial.suggest_float("ss_speed_threshold", 0.0001, 0.1)
-    ar_config["fast_approach"]["speed_threshold"] = trial.suggest_float("fa_speed_threshold", 0.0001, 0.1)
-    ar_config["suddenly_run"]["speed_threshold"] = trial.suggest_float("sr_speed_threshold", 0.0001, 0.1)
+    if include_projection:
+        ar_config["speed_projection"][0] = trial.suggest_float("speed_projection_x", 0.5, 2.0)
+        ar_config["speed_projection"][1] = trial.suggest_float("speed_projection_y", 0.5, 2.0)
+
+    if include_buffer:
+        ar_config["speed_buffer_len"] = trial.suggest_int("speed_buffer_len", 1, 15)
+        ar_config["frame_stride"] = trial.suggest_int("frame_stride", 1, 60)
+
+    if objective == "Macro" or objective == "Micro":
+        ar_config["gather"]["distance_threshold"] = trial.suggest_float("g_distance_threshold", 0.1, 1.5)
+        ar_config["gather"]["area_threshold"] = trial.suggest_float("g_area_threshold", 0.1, 1.0)
+        ar_config["gather"]["speed_threshold"] = trial.suggest_float("g_speed_threshold", 0.0001, 0.1)
+        ar_config["stand_still"]["speed_threshold"] = trial.suggest_float("ss_speed_threshold", 0.0001, 0.1)
+        ar_config["fast_approach"]["speed_threshold"] = trial.suggest_float("fa_speed_threshold", 0.0001, 0.1)
+        ar_config["suddenly_run"]["speed_threshold"] = trial.suggest_float("sr_speed_threshold", 0.0001, 0.1)
+
+    elif objective == "G":
+        ar_config["gather"]["distance_threshold"] = trial.suggest_float("g_distance_threshold", 0.1, 1.5)
+        ar_config["gather"]["area_threshold"] = trial.suggest_float("g_area_threshold", 0.1, 1.0)
+        ar_config["gather"]["speed_threshold"] = trial.suggest_float("g_speed_threshold", 0.0001, 0.1)
+
+        ar_config["stand_still"]["enabled"] = False
+        ar_config["fast_approach"]["enabled"] = False
+        ar_config["suddenly_run"]["enabled"] = False
+
+    elif objective == "SS":
+        ar_config["stand_still"]["speed_threshold"] = trial.suggest_float("ss_speed_threshold", 0.0001, 0.1)
+
+        ar_config["gather"]["enabled"] = False
+        ar_config["fast_approach"]["enabled"] = False
+        ar_config["suddenly_run"]["enabled"] = False
+
+    elif objective == "FA":
+        ar_config["fast_approach"]["speed_threshold"] = trial.suggest_float("fa_speed_threshold", 0.0001, 0.1)
+
+        ar_config["gather"]["enabled"] = False
+        ar_config["stand_still"]["enabled"] = False
+        ar_config["suddenly_run"]["enabled"] = False
+
+    elif objective == "SR":
+        ar_config["suddenly_run"]["speed_threshold"] = trial.suggest_float("sr_speed_threshold", 0.0001, 0.1)
+
+        ar_config["gather"]["enabled"] = False
+        ar_config["stand_still"]["enabled"] = False
+        ar_config["fast_approach"]["enabled"] = False
+
+    elif objective == "projection":
+        ar_config["speed_projection"][0] = trial.suggest_float("speed_projection_x", 0.5, 2.0)
+        ar_config["speed_projection"][1] = trial.suggest_float("speed_projection_y", 0.5, 2.0)
+        objective = "Macro"
+    else:
+        raise ValueError(f"Objective {objective} not recognized")
 
     ###############################
     # PERFORM INFERENCE AND SAVE CONFIG
@@ -54,15 +101,26 @@ def ar_optuna_fitness_fn(trial, config):
 
     # Override the config to include the experiment_id in the pred_dir and disable printing the confusion matrix
     eval_config["pred_dir"] = trackers_folder + "/" + trackers_to_eval + "/"
-    eval_config["action_recognition"]["smoothing_window"] = 0
-    eval_config["action_recognition"]["final_pad"] = 0
+    eval_config["data_dir"] = config["source_gt_dir"] + "/"
+    eval_config["action_recognition"]["smoothing_window"] = trial.suggest_int("smoothing_window", 0, 60, step=10) if "smoothing_window" in param_names else 0
+    eval_config["action_recognition"]["final_pad"] = trial.suggest_int("final_pad", 0, 60, step=10) if "final_pad" in param_names else 0
+    eval_config["action_recognition"]["initial_pad"] = trial.suggest_int("initial_pad", 0, 200, step=10) if "initial_pad" in param_names else 0
     eval_config["action_recognition"]["save_results"] = False
     eval_config["action_recognition"]["print_results"] = False
+    eval_config["action_recognition"]["discriminate_groups"] = False
+
+    if objective in ["SS", "FA", "SR", "G"]:
+        eval_config["action_recognition"]["active_behaviors"] = [objective]
+    else:
+        eval_config["action_recognition"]["active_behaviors"] = ["SS", "FA", "SR", "G"]
 
     evaluator = AREvaluator(eval_config)
     metrics_df = evaluator.evaluate()
 
-    return metrics_df[metrics_df['Class'] == 'Macro']['F2'].values[0]
+    print(f"Trial {trial.number}:")
+    print(metrics_df.to_string(float_format="{:.4f}".format, index=False, col_space=10))
+
+    return metrics_df[metrics_df['Class']==objective]['F'].values[0]
 
 
 def print_and_save(study, trial):
@@ -84,28 +142,53 @@ if __name__ == "__main__":
     with open("./cfg/evolve.json", "r") as f:
         config = json.load(f)
 
-    params_to_optimize = ["speed_projection_x", "speed_projection_y",  "g_distance_threshold", "g_area_threshold",
-                          "ss_speed_threshold", "fa_speed_threshold", "sr_speed_threshold"]
+    objective = config["evolve"]["objective"]
+    include_projection = config["evolve"]["projection"]
 
-    # Enqueue trial for good starting point
-    initial_params = {
-        "speed_projection_x": config["action_recognition"]["speed_projection"][0],
-        "speed_projection_y": config["action_recognition"]["speed_projection"][1],
-        "g_distance_threshold": config["action_recognition"]["gather"]["distance_threshold"],
-        "g_area_threshold": config["action_recognition"]["gather"]["area_threshold"],
-        "g_speed_threshold": config["action_recognition"]["gather"]["speed_threshold"],
-        "ss_speed_threshold": config["action_recognition"]["stand_still"]["speed_threshold"],
-        "fa_speed_threshold": config["action_recognition"]["fast_approach"]["speed_threshold"],
-        "sr_speed_threshold": config["action_recognition"]["suddenly_run"]["speed_threshold"]
-    }
+    initial_params = {}
+    params_to_optimize = []
+    if objective in ["SS", "Macro", "Micro"]:
+        params_to_optimize.append("ss_speed_threshold")
+        initial_params["ss_speed_threshold"] = config["action_recognition"]["stand_still"]["speed_threshold"]
 
-    # TODO: SHOULD WE INCLUDE EVAL HYPERPARAMS IN THE OPTIMIZATION?
+    if objective in ["FA", "Macro", "Micro"]:
+        params_to_optimize.append("fa_speed_threshold")
+        initial_params["fa_speed_threshold"] = config["action_recognition"]["fast_approach"]["speed_threshold"]
+
+    if objective in ["SR", "Macro", "Micro"]:
+        params_to_optimize.append("sr_speed_threshold")
+        initial_params["sr_speed_threshold"] = config["action_recognition"]["suddenly_run"]["speed_threshold"]
+
+    if objective in ["G", "Macro", "Micro"]:
+        params_to_optimize.append("g_distance_threshold")
+        params_to_optimize.append("g_area_threshold")
+        params_to_optimize.append("g_speed_threshold")
+        initial_params["g_distance_threshold"] = config["action_recognition"]["gather"]["distance_threshold"]
+        initial_params["g_area_threshold"] = config["action_recognition"]["gather"]["area_threshold"]
+        initial_params["g_speed_threshold"] = config["action_recognition"]["gather"]["speed_threshold"]
+
+    if include_projection or objective == "projection":
+        params_to_optimize = ["speed_projection_x", "speed_projection_y"] + params_to_optimize
+        initial_params["speed_projection_x"] = config["action_recognition"]["speed_projection"][0]
+        initial_params["speed_projection_y"] = config["action_recognition"]["speed_projection"][1]
+
+    if config["evolve"]["post_process"]:
+        params_to_optimize += ["initial_pad", "final_pad", "smoothing_window"]
+        initial_params["initial_pad"] = 0
+        initial_params["final_pad"] = 0
+        initial_params["smoothing_window"] = 0
+
+    if config["evolve"]["buffer"]:
+        params_to_optimize += ["speed_buffer_len", "frame_stride"]
+        initial_params["speed_buffer_len"] = config["action_recognition"]["speed_buffer_len"]
+        initial_params["frame_stride"] = config["action_recognition"]["frame_stride"]
+
     study.enqueue_trial(initial_params)
 
     # We could add a continuous save function to save the study every 10 trials and print the best trial
     study.optimize(
-        func=partial(ar_optuna_fitness_fn, config=config),
-        n_trials=100,
+        func=partial(ar_optuna_fitness_fn, config=config, objective=objective, param_names=params_to_optimize),
+        n_trials=config["evolve"]["n_trials"],
         show_progress_bar=True,
         callbacks=[print_and_save]
     )
