aa6db7dcc325f3a315e4f0ba638f9b4a0f98f4fc
Johnny <johnnynuca14@gmail.com>
2024-07-18 10:59:27 +0200

    Update nms_wrapper_infer.py

--- inference/nms_wrapper_infer.py
+++ inference/nms_wrapper_infer.py
@@ -6,8 +6,9 @@ import os
 import csv
 
 def nms_wrapper_infer(config):
-    # Read all image paths
     from tracker.jetson.model.model import Yolov8
+
+    # Read all image paths
     image_paths = [str(p) for p in Path(config['input_data_dir']).rglob('*') if p.suffix.lower() in ['.jpg', '.jpeg', '.png', '.bmp']]
 
     # Initialize Yolov8 model with appropriate class names
@@ -24,6 +25,8 @@ def nms_wrapper_infer(config):
     # Create output directory for annotated images, predicted labels and timing results
     output_dir = config['output_dir'] + "/" + time.strftime("%Y%m%d-%H%M%S")
     os.makedirs(output_dir, exist_ok=True)
+    output_labels = output_dir + "/labels/"
+    os.makedirs(output_labels)
 
     timing_results = []
     for img_path in tqdm(image_paths, desc="Processing images with Yolov8"):
@@ -34,13 +37,19 @@ def nms_wrapper_infer(config):
             annotated_img = results[0].plot()  # Assuming `results` is a list of `Results` objects
             save_path = os.path.join(output_dir, os.path.basename(img_path))
             cv2.imwrite(save_path, annotated_img)
-
-            # TODO: Save predicted labels to a text file inside output_dir/labels/<image_name>.txt
-
+            label_path = os.path.join(output_labels, os.path.splitext(os.path.basename(img_path))[0] + '.txt')
+            with open(label_path, 'w') as f:
+                for result in results:
+                    for bbox in results[0].boxes:
+                        class_id = int(bbox.cls.item())
+                        x_center, y_center, width, height = bbox.xywh.flatten().tolist()
+                        confidence = bbox.conf.item()
+                        f.write(f"{class_id} {x_center} {y_center} {width} {height} {confidence}\n")
     # Save inference times to csv
     csv_output_path = os.path.join(output_dir, 'inference_time.csv')
     with open(csv_output_path, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Image Path", "Pre-process (ms)", "Inference (ms)", "Post-process (ms)"])
         for img_path, timing in zip(image_paths, timing_results):
-            writer.writerow([img_path, *timing])
+            image_name = img_path.split('/')[-1].split('.')[0]
+            writer.writerow([image_name, *timing])
